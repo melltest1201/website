6323d4fb69d5a336c8a4d06d238304b5999fc4c5
melltest1201 <melltest1201@gmail.com>
2023-03-14 18:47:56 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
-import { Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import EC from "./components/payments/Ec";
 import PA from "./components/payments/Pa";
 
 function App() {
   return (
-    <Routes>
-      {/* Rutas aqui­ */}
-      <Route path="/components/payments/Ec" element={<EC />} />
-      <Route path="/components/payments/PA" element={<PA />} />
-    </Routes>
+    <BrowserRouter basename="/website">
+      <Routes>
+        {/* Rutas aqui­ */}
+        <Route path="/components/payments/Ec" element={<EC />} />
+        <Route path="/components/payments/PA" element={<PA />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
